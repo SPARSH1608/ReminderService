eda0594525117bbb82bac487449bb3cc7aee0ea0
Sparsh <sparshgoelk@gmail.com>
2024-09-04 10:38:59 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cron = require('node-cron');
 
 const { PORT } = require('./config/serverConfig');
 const { sendBasicEmail } = require('./services/email-service');
@@ -12,12 +13,15 @@ const setupAndStart = () => {
   app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 
-    sendBasicEmail(
-      'support123@gmail.com',
-      'its.sparshh@gmail.com',
-      'This is testing email',
-      'Hey, how are you'
-    );
+    // sendBasicEmail(
+    //   'support123@gmail.com',
+    //   'its.sparshh@gmail.com',
+    //   'This is testing email',
+    //   'Hey, how are you'
+    // );
+    cron.schedule('* * * * *', () => {
+      console.log('running a task every minute');
+    });
   });
 };
 setupAndStart();

72ce8c2f57ad49d2d6a51f266438de7348c1ff79
Sparsh <sparshgoelk@gmail.com>
2024-09-04 10:01:46 +0530

    setup of smtp server

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const { sendBasicEmail } = require('./services/email-service');
+
 const setupAndStart = () => {
   const app = express();
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+
   app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
+
+    sendBasicEmail(
+      'support123@gmail.com',
+      'its.sparshh@gmail.com',
+      'This is testing email',
+      'Hey, how are you'
+    );
   });
 };
 setupAndStart();
---
+++ src/services/email-service.js
@@ -0,0 +1,17 @@
+const sender = require('../config/emailConfig');
+
+const sendBasicEmail = (mailFrom, mailTo, mailSubject, mailBody) => {
+  try {
+    sender.sendMail({
+      from: mailFrom,
+      to: mailTo,
+      subject: mailSubject,
+      text: mailBody,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+module.exports = {
+  sendBasicEmail,
+};
